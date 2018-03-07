79097c5e092ae3a6aa4b29d763cfde82329f08b7
Stanley Cheah <sccheah@ucdavis.edu>
2018-03-06 21:32:25 -0800

    comment modification and added csv files

--- maxTweeter.c
+++ maxTweeter.c
@@ -4,24 +4,22 @@
 
 int main(int argc, char** argv)
 {
-	//ssize_t somechar;
 	char c;
-	//char* line = NULL;
-	//size_t len = 0;
 	FILE* fp;
 	fp = fopen(argv[1], "r");
 	char line[1024];			// longest length in csv file is 459, so to be safe, we set to 1024 for now
-    // char line[10000000]
-    
+
+	// test if path does not exist and exit if it doesn't
 	if (fp == NULL)
 	{
 		printf("Invalid Input Format\n");
-		exit(-1); // exit -1 means exit with failure
+		exit(-1);
 	}
 
+	// get each line from file and split according to token
 	while (fgets(line, 1024, fp))
 	{
-		char* token = strtok(line, ",");
+		char *token = strtok(line, ",");
 
 		while (token != NULL)
 		{
@@ -30,23 +28,26 @@ int main(int argc, char** argv)
 		}
 	}
 
-
-	/*
-	//while ((somechar = getline(&line, &len, fp) != 1))
-	//	printf("%s", line); // this chunk stops when it encounters a new line
-
-	c = fgetc(fp);
-	while (c != EOF)
-	{
-		printf("%c", c);
-		c = fgetc(fp);
-	} // this prints the entire file
-
-	printf("\n");
-	fclose(fp);
-	*/
-
-
 	return 0;
 
 }
+
+
+
+
+
+
+/*
+//while ((somechar = getline(&line, &len, fp) != 1))
+//	printf("%s", line); // this chunk stops when it encounters a new line
+
+c = fgetc(fp);
+while (c != EOF)
+{
+	printf("%c", c);
+	c = fgetc(fp);
+} // this prints the entire file
+
+printf("\n");
+fclose(fp);
+*/
